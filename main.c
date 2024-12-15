1a6f4fef88561103fbce03fa29a0f0a259d2e861
Issac-Maza <issamaza@espol.edu.ec>
2024-12-15 00:48:07 -0500

    Agregando main

---
+++ main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void mostrarMenu() {
+	printf("\n  MENU PRINCIPAL   \n");
+	printf("1. Administrar las Materias\n");
+	printf("2. Administrar los Estudiantes\n");
+	printf("3. Administrar los Profesores\n");
+	printf("4. Administrar los Cursos\n");
+	printf("5. Salir\n");
+	printf("Opcion a elegir: ");
+
+}
+
+int main (){
+	int opcion;
+	do{
+		mostrarMenu();
+		scanf("%d",&opcion);
+		switch(opcion){
+		case 1:
+			break;
+		case 2:
+                	break;
+		case 3:
+                	break;
+		case 4:
+                	break;
+		case 5:
+			printf("Saliendo del programa\n");
+                	break;
+		default:
+			printf("Opcion invalida o incorrecta \n");
+			break;
+		}
+	}while(opcion != 5 );
+
+	return 0;
+}
