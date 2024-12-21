68cccc351cc67ce8d10164d8902bc4f58067811d
Issac-Maza <84421657+Issac-Maza@users.noreply.github.com>
2024-12-20 22:25:36 -0500

    Update materias.h
    comentarios

--- materias.h
+++ materias.h
@@ -3,26 +3,26 @@
 
 #include <stdbool.h>
 
-// Estructura para una materia
+
 typedef struct {
     char nombre[50];
     char codigo[10];
     bool estado; // true = Activo, false = Inactivo
 } Materia;
 
-// Declaraciones globales para el arreglo dinámico
-extern Materia *materias; // Puntero dinámico a Materia
-extern int totalMaterias; // Número actual de materias
-extern int capacidadMaterias; // Capacidad actual del arreglo dinámico
 
-// Funciones relacionadas con las materias
-void inicializarMaterias(); // Inicializa las materias desde un archivo
-void guardarMaterias();     // Guarda las materias en un archivo
-void crearMateria();        // Agrega una nueva materia
-void editarMateria();       // Edita una materia existente
-void listarMaterias();      // Lista todas las materias
-bool validarCodigoUnico(const char *codigo); // Verifica que el código sea único
-void liberarMaterias();     // Libera la memoria dinámica utilizada por el arreglo
+extern Materia *materias; 
+extern int totalMaterias; 
+extern int capacidadMaterias; 
+
+
+void inicializarMaterias(); 
+void guardarMaterias();     
+void crearMateria();        
+void editarMateria();       
+void listarMaterias();      
+bool validarCodigoUnico(const char *codigo); 
+void liberarMaterias();     
 
 int obtenerTotalMaterias();
 const Materia *obtenerMateriaPorIndice(int i);
